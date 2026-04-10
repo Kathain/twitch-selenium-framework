77d63a2deb6d19a373bcbda7550de96292c5d1f8
Katharina Kraus <kkraus132@gmail.com>
2026-04-10 15:51:33 +0200

    Add GitHub Actions CI and Allure reports

--- utils/drivers.py
+++ utils/drivers.py
@@ -6,6 +6,9 @@ def get_chrome_driver():
     mobile_emulation = {"deviceName": "Pixel 7"}
 
     chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_experimental_option("mobileEmulation", mobile_emulation)
 
     driver = webdriver.Chrome(options=chrome_options)
